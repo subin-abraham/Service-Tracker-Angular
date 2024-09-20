045a816cf1189495bb317d8cea6aedd513be57e9
subin-abraham <subinkunnuvilayil@gmail.com>
2024-09-20 16:26:35 +0530

    DB Connection for registration

--- src/app/Components/registration-page/registration-page.component.ts
+++ src/app/Components/registration-page/registration-page.component.ts
@@ -3,28 +3,39 @@ import { ServicesService } from '../../services.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.css'
 })
 export class RegistrationPageComponent {
+  ngOnInit() {
+    this.getRegisteredUsers();    
+  }
+  apiUrl = 'http://localhost:3000';
   service = inject(ServicesService);
   router = inject(Router);
+  http = inject(HttpClient);
   // Registration Variables
   name: string = "";
   userName: string = "";
   password: string = "";
+  registeredUsers: any[] = [];
   registerSuccessMessage = ""
   registerErrorMessage = ""
 
   register() {
-    this.service.register(this.name, this.userName, this.password).subscribe((response) => {
+    const registered_users = {
+      name: this.name, username: this.userName, password: this.password
+    }
+    this.http.post(`${this.apiUrl}/New-Registration`, registered_users).subscribe((response) => {
       this.registerSuccessMessage = 'Registration successful. You can now log in!';
       this.registerErrorMessage = '';
+      this.getRegisteredUsers();
       this.clearRegistrationFields();
       setTimeout(() => {
         this.router.navigate(['/']);
@@ -40,4 +51,15 @@ export class RegistrationPageComponent {
     this.userName = "";
     this.password = "";
   }
+  getRegisteredUsers() {
+    this.http.get<any[]>(`${this.apiUrl}/registered_user`).subscribe(
+      users => {
+        this.registeredUsers = users;
+      },
+      error => {
+        console.error('Failed to fetch users', error);
+      }
+    );
+  }
 }
+
